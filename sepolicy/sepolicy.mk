a7f97c23a669c78467cf9e62bd225c3a5f448bb1
Yashar Ahmedizade <yashade2001@gmail.com>
2015-06-17 18:11:52 +0200

    Welcome sepolicy files in vendor
    Change-Id: Iad1c03b7c184bb8e2cb084056f3ad1262341a762

---
+++ sepolicy/sepolicy.mk
@@ -0,0 +1,30 @@
+#
+# This policy configuration will be used by all products that
+# inherit from CM
+#
+
+BOARD_SEPOLICY_DIRS += \
+    vendor/aosp/sepolicy
+
+BOARD_SEPOLICY_UNION += \
+    file.te \
+    file_contexts \
+    genfs_contexts \
+    property_contexts \
+    seapp_contexts \
+    service_contexts \
+    auditd.te \
+    healthd.te \
+    hostapd.te \
+    installd.te \
+    netd.te \
+    property.te \
+    shell.te \
+    sysinit.te \
+    system.te \
+    system_app.te \
+    ueventd.te \
+    uncrypt.te \
+    userinit.te \
+    vold.te \
+    mac_permissions.xml
